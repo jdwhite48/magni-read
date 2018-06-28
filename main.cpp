e51dcdf6c4fce2d1951869aad1bf5b2ace685600
jdwhite88 <white570@purdue.edu>
2018-06-27 22:05:32 -0400

    Increase default font size to 18 pt

--- main.cpp
+++ main.cpp
@@ -4,6 +4,13 @@
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+
+    // Use large font (18 point size, system default if larger)
+    QFont defaultFont = a.font();
+    QFont _font(defaultFont.family(),
+                (defaultFont.pointSize() > 18) ? defaultFont.pointSize() : 18);
+    a.setFont(_font);
+
     MainWindow w;
     w.show();
 
