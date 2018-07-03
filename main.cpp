5902394ededd97876cb6d8ea10178128b5ef834c
jdwhite88 <white570@purdue.edu>
2018-07-02 23:35:03 -0400

    Use external stylesheet

--- main.cpp
+++ main.cpp
@@ -1,5 +1,10 @@
 #include "mainwindow.h"
+
 #include <QApplication>
+#include <QFile>
+#include <QString>
+
+#include <iostream>
 
 int main(int argc, char *argv[])
 {
@@ -8,9 +13,18 @@ int main(int argc, char *argv[])
     // Use large font (18 point size, system default if larger)
     QFont defaultFont = a.font();
     QFont _font(defaultFont.family(),
-                (defaultFont.pointSize() > 18) ? defaultFont.pointSize() : 18);
+                (defaultFont.pointSize() > 18) ? defaultFont.pointSize() : 18); 
     a.setFont(_font);
 
+    // Use custom stylesheet
+    QFile file("stylesheet.css");
+    if (file.open(QFile::ReadOnly)) {
+        QString stylesheet(file.readAll());
+        // Replace whitespace characters with normal space
+        stylesheet = stylesheet.simplified();
+        a.setStyleSheet(stylesheet);
+    }
+    file.close();
     MainWindow w;
     w.show();
 
